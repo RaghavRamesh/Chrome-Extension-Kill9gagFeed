b07bcbdf6192202ab686d6cef308d1e3463f9aef
Raghav Ramesh <--add>
2014-06-06 15:17:17 +0800

    refactor: remove debug comments

--- pages/script.js
+++ pages/script.js
@@ -1,11 +1,10 @@
 function killNewsFeed() {
 	var feed = document.getElementById("list-view-2");
 	feed.className = "hide";
-	console.log(feed);
 
 	var sidebar = document.getElementById("sidebar");
 	sidebar.className = "hide";
-	console.log(sidebar);
+
 };
 
 
