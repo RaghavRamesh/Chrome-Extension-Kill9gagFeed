ec9c38a170cc3bef6801178ac9b1e71a286ad103
Raghav Ramesh <--add>
2014-06-06 14:01:14 +0800

    feat: killNewsFeed() - obtains the feed and the sidebar and hides them

--- pages/script.js
+++ pages/script.js
@@ -1,1 +1,12 @@
-alert("Hello World!");
+function killNewsFeed() {
+	var feed = document.getElementById("list-view-2");
+	feed.className = "hide";
+	console.log(feed);
+
+	var sidebar = document.getElementById("sidebar");
+	sidebar.className = "hide";
+	console.log(sidebar);
+};
+
+
+killNewsFeed();
